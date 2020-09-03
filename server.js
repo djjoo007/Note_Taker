cb74d71d3d8ed09c0cb57afdf96d40f48242bbb9
Joo Heredia <djjoo007uw@gmail.com>
2020-09-03 13:58:22 -0700

    add post route

--- server.js
+++ server.js
@@ -7,61 +7,42 @@ const noteJSON = require("./db/db.json");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Set up Express app to handle data parsing
+// Set up Express App to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 
 // ROUTES
-// html route Notes Page
+// Html Route to Notes Page
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
   
-// html route Home Page
+// Html Route to Home Page
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
   
-// api route
+// API Route Get Notes
 app.get("/api/notes", function(req, res) {
     res.json(noteJSON);
 });
 
-//   Post Request
-app.post("/api/notes", function(req, res) {
-    const newNotes = req.body;
-    const id = noteJSON.length;
-    newNotes.id = id +1;
-    noteJSON.push(newNotes);
-    noteUpdate(noteJSON);
-    return res.json(noteJSON);
-});
-
-// app.get("/api/notes", function(req, res) {
-//     fs.readFile("./db/db.json", "utf-8", function(data) {
-//         return res.json(data);
-//     });
-// });
-
-// app.get("/api/notes", function(req, res) {
-//         // fs.readFile("./db/db.json", "utf8", function() {
-//         //     return res.json(db);
-//         // });
-//         console.log(db);
-//       return res.json(db);
-//   });
-
-
-
-// Delete Request
-app.delete("/api/notes/:id", function(req, res) {
-    const id = req.params.id;
-    const note
-});
-
-
+// API Route Post Notes
+app.post('/api/notes', (req, res) => {
+  const lastId = noteJSON.length ? Math.max(...(noteJSON.map(note => note.id))) : 0;
+  const id = lastId + 1;
+  noteJSON.push( { id, ...req.body} );
+  res.json(noteJSON.slice(-1));
+  });
+  
+// Delete Request 
+  app.delete('/api/notes/:id', (req, res) => {
+  const note = noteJSON.find( ({ id }) => id === JSON.parse(req.params.id));
+  noteJSON.splice( noteJSON.indexOf(note), 1);
+  res.end();
+  });
 
   // Starts the server to begin listening
 app.listen(PORT, function() {
